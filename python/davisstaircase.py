1bbd60b987754452c070d662af33a96196e4e637
freddie_rav <freddie.rav@gmail.com>
2022-06-08 21:10:51 -0500

    davisstaircase final form

--- python/davisstaircase.py
+++ python/davisstaircase.py
@@ -7,16 +7,14 @@ import re
 import sys
 
 def runStair(n, posibleSteps, currentPath):
-    pathsTaken = []
+    pathsTaken = 0
 
     for step in posibleSteps:
         count = sum(currentPath + [step])
         if count < n:
             pathsTaken += runStair(n, posibleSteps, currentPath + [step])
         elif count == n:
-            pathsTaken.append(currentPath + [step])
-
-    # print(pathsTaken)
+            pathsTaken += 1
 
     return pathsTaken
 
@@ -24,7 +22,7 @@ def stepPerms(n):
     posibleSteps = [1, 2, 3]
     stepsTaken = runStair(n, posibleSteps, [])
 
-    return len(stepsTaken) % (pow(10, 10) + 7)
+    return stepsTaken % (pow(10, 10) + 7)
 
 
-print(stepPerms(5))
+print(stepPerms(27))
